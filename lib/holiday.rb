51152aa41563542f05dcea3679e31cfc42a863b8
Sonja Parsell <hello@sparsell.com>
2020-04-07 15:48:23 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,32 +1,43 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
-end
+holiday_hash = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
+# def second_supply_for_fourth_of_july(holiday_hash)
+#   # return the second element in the 4th of July array
+#   return holiday_hash.dig :summer, :fourth_of_july, 1
+# end
+
+# add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  def add_supply_to_winter_holidays(holiday_hash, supply)
 
-end
+    holiday_hash.each do |season, holidays|
+    if season == "winter"
+      binding.pry
+      holidays.each do |attribute, list|
 
+        #items << supply
+
+          end
+        end
+      end
+    end
+
+add_supply_to_winter_holidays(holiday_hash, "champagne")
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
@@ -61,10 +72,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
